88b9b4cd6eb6f7b0bdc2e6b0bd9df1fe675954e2
Sally Poulsen <sallypoulsen@gmail.com>
2014-07-04 16:26:25 -0600

    basic layout with plugin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+// Docs at http://simpleweatherjs.com
+$(function() {
+  $.simpleWeather({
+    location: 'Edmonton, Alberta',
+    unit: 'c',
+    success: function(weather) {
+      html = '<p class="weather-title">'+weather.title+'</p>';
+	  html += '<i class="fa fa-sun-o"></i>';
+      html += '<p class="temperature">'+weather.temp+' &deg;C &amp; ' +weather.currently;
 
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error.message+'</p>');
+    }
+  });
+});
