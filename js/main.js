e829dfc6e5768ad7bec06075f5fe26089ffb55d4
Sally Poulsen <sallypoulsen@gmail.com>
2014-07-04 16:36:25 -0600

    Adds colors.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-// Docs at http://simpleweatherjs.com
-$(function() {
-  $.simpleWeather({
-    location: 'Edmonton, Alberta',
-    unit: 'c',
-    success: function(weather) {
-      html = '<p class="weather-title">'+weather.title+'</p>';
-	  html += '<i class="fa fa-sun-o"></i>';
-      html += '<p class="temperature">'+weather.temp+' &deg;C &amp; ' +weather.currently;
 
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error.message+'</p>');
-    }
-  });
+
+$(document).ready(function(){
+	
+	//simpleweather
+	// Docs at http://simpleweatherjs.com
+	$(function() {
+	  $.simpleWeather({
+	    location: 'Edmonton, Alberta',
+	    unit: 'c',
+	    success: function(weather) {
+	      html = '<p class="weather-title">'+weather.title+'</p>';
+		  html += '<i class="fa fa-sun-o"></i>';
+	      html += '<p class="temperature">'+weather.temp+' &deg;C &amp; ' +weather.currently;
+
+	      $("#weather").html(html);
+	    },
+	    error: function(error) {
+	      $("#weather").html('<p>'+error.message+'</p>');
+	    }
+	  });
+	});
+	
+	
+	//color transition
+	
+	var date = new Date();
+	var hour = date.getHours();
+	var minutes = date.getMinutes();
+	
+	var time = hour + ':' + minutes;
+	
+	alert(time);
+	
+	
 });
